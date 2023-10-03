efc2cd432febca88001511a880f2f7a6c455e9fa
akram-tello <a.tello@graduate.utm.my>
2023-10-03 18:03:49 +0800

    fix logos section

--- app/public/wp-content/themes/fundedx/blocks/module-logos.php
+++ app/public/wp-content/themes/fundedx/blocks/module-logos.php
@@ -1,15 +1,13 @@
 <?php 
     $className = !empty($block['className']) ? $block['className'] : null;
-    $heading        = get_field('heading');
+    $heading   = get_field('heading');
 ?>
-
-
 <section class="module module--logo-carousel bg-gray <?= $className ?>">
-    <div class="wrapper">
-        <h2 class="module--title text-center"><?= $heading ?></h2>
-        <div class="logo-carousel js-logo-carousel owl-carousel">
+    <div class="wrapper px-4 py-6 mx-auto max-w-7xl">
+        <h2 class="module--title text-center mb-6 text-xl font-bold"><?= $heading ?></h2>
+        <div class="logos flex flex-wrap justify-center items-center gap-y-2 gap-x-4 md:gap-4 md:space-y-0 md:flex-nowrap md:overflow-x-auto">
             <?php while( have_rows('logo') ): the_row(); ?>
-                <div class="logo-item">
+                <div class="logo-item flex flex-col items-center mb-4 md:mb-0">
                     <?php 
                         $logo  = get_sub_field('logo');
                         $link  = get_sub_field('link');
@@ -17,12 +15,12 @@
                     ?>
 
                     <?php if( !empty( $link ) ): ?>
-                        <a href="<?= $link ?>" target="_blank">
+                        <a href="<?= $link ?>" target="_blank" class="flex flex-col items-center">
                     <?php endif ?>
-                        <img data-src="<?= $logo['url'] ?>" src="<?= get_template_directory_uri() ?>/img/placeholder.png" alt="<?= $logo['alt'] ?>">
+                        <img data-src="<?= $logo['url'] ?>" src="<?= get_template_directory_uri() ?>/img/placeholder.png" alt="<?= $logo['alt'] ?>" class="w-24">
 
                         <?php if( !empty( $title ) ): ?>
-                            <p><?= $title ?></p>
+                            <p class="mt-2 text-sm"><?= $title ?></p>
                         <?php endif ?>
                     <?php if( !empty( $link ) ): ?>
                         </a>
@@ -33,3 +31,30 @@
     </div>
 </section>
 
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        // Initialize Owl Carousel for desktop view
+        if (window.matchMedia("(min-width: 768px)").matches) {
+            $('.logos').addClass('owl-carousel owl-theme').owlCarousel({
+                items: 5,
+                loop: true,
+                margin: 10,
+                autoplay: true,
+                autoplayTimeout: 3000,
+                autoplayHoverPause: true,
+                responsiveClass: true,
+                responsive:{
+                    0:{
+                        items:1,
+                    },
+                    600:{
+                        items:3,
+                    },
+                    1000:{
+                        items:5,
+                    }
+                }
+            });
+        }
+    });
+</script>
