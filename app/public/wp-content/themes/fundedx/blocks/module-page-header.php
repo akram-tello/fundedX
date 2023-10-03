5f6fb59b997bca98d52b54ef3470c17a1ce4c4ff
akram-tello <a.tello@graduate.utm.my>
2023-10-03 16:39:36 +0800

    update page-header

--- app/public/wp-content/themes/fundedx/blocks/module-page-header.php
+++ app/public/wp-content/themes/fundedx/blocks/module-page-header.php
@@ -18,26 +18,53 @@
                 <h1 class="module--title"><?= $heading ?></h1>
                 <?= $content ?>
                 <?php if( !empty( $cta ) ): ?>
-                    <a href="<?= $cta['url'] ?>" class="btn btn--light mt-30px"><?= $cta['title'] ?> <?= get_template_part('img/arrow-up.svg'); ?></a>
-                <?php endif ?>
+                    <div class="cta-center">
+                        <a href="<?= $cta['url'] ?>" class="btn btn--light mt-30px"><?= $cta['title'] ?> <?= get_template_part('img/arrow-up.svg'); ?></a>
+                    </div>
+                    <?php endif ?>
+                <img id="image-phone" data-src="<?= get_template_directory_uri(); ?>/img/phone.png" src="<?= get_template_directory_uri() ?>/img/placeholder.png" alt="" style="height: 22rem; margin: auto; margin-top: 1rem">
             </div>
 
             <?php if(is_page('home')) : ?>
 
-                <div class="stats-holder block md:flex flex-wrap mt-80px">
-                    <?php if( have_rows('stats') ): ?>
-                        <?php while( have_rows('stats') ): the_row(); ?>
-                            <div class="stats-list text-light mr-0 md:mr-40px">
-                                <p><?php the_sub_field('title'); ?></p>
-                                <h3><?php the_sub_field('number'); ?></h3>
-                            </div>
-                        <?php endwhile; ?>
-                    <?php endif; ?>
-                </div>
+            <div class="stats-holder flex space-x-4 md:space-x-0 md:flex-wrap mt-80px">
+                <?php if( have_rows('stats') ): ?>
+                    <?php while( have_rows('stats') ): the_row(); ?>
+                        <div class="stats-list text-light mr-0 md:mr-40px">
+                            <p><?php the_sub_field('title'); ?></p>
+                            <h3><?php the_sub_field('number'); ?></h3>
+                        </div>
+                    <?php endwhile; ?>
+                <?php endif; ?>
+            </div>
 
             <?php endif; ?>
 
         </div>
     </div>
 </section>
+
+
+
+<script>
+const mediaQuery = window.matchMedia('(max-width: 639px)');
+
+const toggleVisibility = (e) => {
+    const imagePhone = document.getElementById('image-phone');
+    const stats = document.getElementById('stats');
+
+    if (e.matches) {
+        if (imagePhone) imagePhone.style.display = 'block';
+        if (stats) stats.style.display = 'block';
+    } else {
+        if (imagePhone) imagePhone.style.display = 'none';
+        if (stats) stats.style.display = 'none';
+    }
+};
+
+toggleVisibility(mediaQuery);
+
+mediaQuery.addListener(toggleVisibility);
+
+</script>
 
